301f6bb3c673da5f718917e28d9030b0c6d962cc
Liam Treacy <liam.treacy@gmail.com>
2021-05-25 16:30:06 +0100

    DayOne tests ready

--- src/main/scala/DayOne.scala
+++ src/main/scala/DayOne.scala
@@ -1,12 +1,31 @@
+import scala.util.control.Breaks._
+
 // Find the two entries that sum to 2020; what do you get if you multiply them together?
 object DayOne {
   def GetDayOneAnswer: Unit = {
     val contents = DataReader.sumValuesFromFile("")
 
-    val positions = 
+    val positions = 1
+    var entries = DataReader.readFile("").toList.map(_.toString.toInt)
+    val (first, second) = FindEntriesWhichSumToExpectedValue(entries, 2020)
   }
 
-  def FindEntriesWhichSumToExpectedValue( contents: List[String], value: Int): Int, Int = {
-    
+  def FindEntriesWhichSumToExpectedValue( contents: List[Int], value: Int) = {
+    var first, second = -1
+    breakable {
+      for (i <- 0 until contents.length)
+      {
+        for( j <- i+1 until contents.length)
+        {
+          if( contents(i) + contents(j) == value)
+          {
+            first = i
+            second = j
+            break
+          }
+        }
+      }
+    }
+    (first,second)
   }
 }
--- src/test/scala/DayOneSpec.scala
+++ src/test/scala/DayOneSpec.scala
@@ -1,6 +1,20 @@
-import org.scalatest.wordspec._
+class DayOneSpec extends org.scalatest.funsuite.AnyFunSuite {
+  test("DayOne.FindEntriesWhichSumToExpectedValue empty list") {
+    assert(DayOne.FindEntriesWhichSumToExpectedValue(List(), 10) equals (-1,-1))
+  }
 
-class DayOneSpec extends AnyWordSpec {
+  test("DayOne.FindEntriesWhichSumToExpectedValue too high a value to reach") {
+    val l = List(1,5,7,8)
+    assert(DayOne.FindEntriesWhichSumToExpectedValue(l, 30) equals (-1,-1))
+  }
 
+    test("DayOne.FindEntriesWhichSumToExpectedValue gets expected value") {
+    val l = List(1,5,7,8,13,15)
+    assert(DayOne.FindEntriesWhichSumToExpectedValue(l, 13) equals (1,3))
+  }
 
+    test("DayOne.FindEntriesWhichSumToExpectedValue gets expected value again") {
+    val l = List(1,5,7,6,8,13,15,2)
+    assert(DayOne.FindEntriesWhichSumToExpectedValue(l, 10) equals (4,7))
+  }
 }
